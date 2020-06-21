db853654f18d9edc417629a4b235e0c80be4802e
Ozgur <49453238+tiskayaozgur@users.noreply.github.com>
2020-06-21 13:15:24 +0300

    My First Homework

---
+++ First_Homework.py
@@ -0,0 +1,55 @@
+#HOMEWORK ANSWERS
+
+"""HOMEWORK 1
+
+a=['selami','ali','veli',10]
+b=[10,'veli','ali','selami']
+#print(a.reverse()==b)
+a.reverse()
+print(a==b)"""
+
+###########################
+
+
+"""HOMEWORK 2:
+a=[10,20,30,40,50,60,70,80,90,100]
+a=a[5:]+a[:5]
+print(a)"""
+#####################################
+
+"""HOMEWORK 3
+
+str=input('Virgüller ile isim listesini giriniz=')
+str=str.split(',')
+print(type(str))"""
+########################################
+
+"""HOMEWORK 4
+
+n=input("Tek basamakli bir sayi giriniz=")
+n1=n+n
+n2=n+n+n
+n3=n+n+n+n
+n4=n+n+n+n+n
+
+print(int(n)+int(n1)+int(n2)+int(n3)+int(n4))"""
+############################################
+"""HOMEWORK 5
+sayi=input("Üc basamakli bir sayi giriniz")
+
+l=list(sayi)
+yüzler=ord(l[0])
+onlar=ord(l[1])
+birler=ord(l[2])
+print(yüzler*100+onlar*10+birler)"""
+#############################################
+"""HOMEWORK 6
+word=input("Bir yazi giriniz=")
+number=int(input("Bir sayi giriniz="))
+
+result=word.center(number,'x')
+print(result)"""
+######################################
+
+
+
